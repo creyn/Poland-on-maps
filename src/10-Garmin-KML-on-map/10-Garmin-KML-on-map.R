fac9a6c24983d992c43ab78754fec7a2687cc37c
creyn <creyn@users.noreply.github.com>
2024-04-01 23:39:47 +0200

    Playing with Garmin KML data.

--- src/10-Garmin-KML-on-map/10-Garmin-KML-on-map.R
+++ src/10-Garmin-KML-on-map/10-Garmin-KML-on-map.R
@@ -1,64 +1,45 @@
-# install.packages("leaflet", repos='http://cran.us.r-project.org')
-
+# # install.packages("leaflet", repos='http://cran.us.r-project.org')
+#
 library(sf)
 library(tidyverse)
 library(leaflet)
 
 setwd(here::here())
 
-# kml_1 <- "data/Garmin/my_activities_kml/14517375977.kml"
-# kml_2 <- "data/Garmin/my_activities_kml/14657867151.kml"
+# test mapping 3 KMLs
+kml_1 <- "data/Garmin/my_activities_kml/14638534217.kml"
+kml_2 <- "data/Garmin/my_activities_kml/14517375977.kml"
+kml_3 <- "data/Garmin/my_activities_kml/14555388589.kml"
+track_1 <- st_read(kml_1, layer = last(st_layers(kml_1)$name))
+track_2 <- st_read(kml_2, layer = last(st_layers(kml_2)$name))
+track_3 <- st_read(kml_3, layer = last(st_layers(kml_3)$name))
+# tracks <- c(track_1$geometry, track_2$geometry, track_3$geometry)
+#
+# map <- leaflet() |>
+#   addProviderTiles(providers$CartoDB.Positron) |>
+#   addPolylines(data = tracks)
+# map
 
+# map all KMLs
 data_path <- "data/Garmin/my_activities_kml/"
 all_kml_files <- list.files(
     path = data_path,
     pattern = ".kml",
     full.names = T
 )
-# all_kml_files
 
-# map <- leaflet() |>
-#   addProviderTiles(providers$CartoDB.Positron)
+# to get the proper type of tracks vector
+first_kml <- all_kml_files[[1]]
+first_track <- st_read(first_kml, layer = last(st_layers(first_kml)$name))
+tracks <- first_track$geometry
 
-all_tracks <- all_kml_files |>
-  lapply(function(x) st_read(x, layer = last(st_layers(x)$name)))
-  # apply(function(x) addPolylines(map, data = x))
+for (kml_file in all_kml_files) {
+  track <- st_read(kml_file, layer = last(st_layers(kml_file)$name))
+  tracks <- c(tracks, track$geometry)
+}
 
-print('all tracks')
-utrack <- unlist(all_tracks)
-
-# for (track in all_tracks) {
-#   addPolylines(map, data = track)
-#   break
-# }
-
-map <- leaflet() |>
-  addProviderTiles(providers$CartoDB.Positron) |>
-  addPolylines(data = utrack)
+map <- leaflet(tracks) |>
+  addProviderTiles(providers$CartoDB.PositronNoLabels) |>
+  addPolylines()
 
 map
-
-# addpol
-#
-
-#
-# for (kml_file in all_kml_files) {
-#   layers <- st_layers(kml_file)
-#   track <- st_read(kml_file, layer = last(layers$name))
-#   addPolylines(map, data = track)
-#   # map |> addPolylines(data = track)
-# }
-# # kml_path <- "data/Garmin/activity_run_20240324.kml"
-# # layers_kml_1 <- st_layers(kml_1)
-# # layers_kml_2 <- st_layers(kml_2)
-#
-# # single_layer_name <- layers[layers$features == 1]
-# # single_layer_name <- last(layers)
-# # single_layer_name$layer_name
-#
-# # track_1 <- st_read(kml_1, layer = last(layers_kml_1$name))
-# # track_2 <- st_read(kml_2, layer = last(layers_kml_2$name))
-# #
-# # track <- st_join(track_2, track_1, join = st_nearest_feature, left = T)
-#
-# map
