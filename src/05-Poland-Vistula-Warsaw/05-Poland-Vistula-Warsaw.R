020a5995b62c01901d51f6a8ea54746ca170d1e2
creyn <creyn@users.noreply.github.com>
2024-04-29 18:33:00 +0200

    Tidy 05 script

--- src/05-Poland-Vistula-Warsaw/05-Poland-Vistula-Warsaw.R
+++ src/05-Poland-Vistula-Warsaw/05-Poland-Vistula-Warsaw.R
@@ -1,90 +1,28 @@
 print("Create map of Poland with Vistula and Warsaw")
 
-print(">>>>> Setup env...")
-packages <- c("sf", "here", "tidyverse", "giscoR")
-installed_packages <- packages %in% rownames(
-    installed.packages()
+install.packages("src/packages/POMUtils_1.0.tar.gz")
+folders <- POMUtils::setup(
+  script_folder = "src/05-Poland-Vistula-Warsaw/",
+  data_folder = "data/",
+  packages = c("sf", "here", "tidyverse", "giscoR")
 )
-if (any(installed_packages == F)) {
-    install.packages(
-        packages[!installed_packages], repos='http://cran.us.r-project.org'
-    )
-}
-invisible(lapply(
-    packages, library,
-    character.only = T
-))
-setwd(here::here())
-
-print(">>>>> Setup folders...")
-script_folder <- "src/05-Poland-Vistula-Warsaw/"
-data_folder <- paste(script_folder, "script-big-data", sep = "/")
-env_data_folder <- Sys.getenv("POM_DATA_FOLDER")
-if(env_data_folder != "") {
-	data_folder <- env_data_folder
-}
-dir.create(data_folder)
-print(paste(">>>>> Data folder setup to: ", data_folder))
-
-output_maps_folder <- script_folder
-env_maps_folder <- Sys.getenv("POM_OUTPUT_MAPS_FOLDER")
-if(env_maps_folder != "") {
-	output_maps_folder <- env_maps_folder
-}
-dir.create(output_maps_folder)
-print(paste(">>>>> Output maps folder setup to: ", output_maps_folder))
 
 print(">>>>> downloading rivers.....")
-dataset_url_rivers <- "http://envirosolutions.pl/dane/rzekiPL.zip"
-dataset_path_rivers <- paste(data_folder, basename(dataset_url_rivers), sep = "/")
-dataset_folder_rivers <- sub(".zip", "", paste(data_folder, basename(dataset_url_rivers), sep = "/"))
-
-if (! file.exists(dataset_path_rivers)) {
-	download.file(
-		url = dataset_url_rivers,
-		destfile = dataset_path_rivers,
-		mode = "wb"
-	)
-
-	unzip(
-		paste(data_folder, basename(dataset_url_rivers), sep = "/"),
-		exdir = dataset_folder_rivers
-	)
-}
-
-filename_rivers <- list.files(
-    path = dataset_folder_rivers,
-    pattern = ".shp",
-    full.names = T
+dataset_rivers <- POMUtils::fetch_zip_with_shp(
+  env_folders = folders,
+  dataset_url = "http://envirosolutions.pl/dane/rzekiPL.zip"
 )
+rivers <- st_read(dataset_rivers, options = "ENCODING=UTF8")
 
 print(">>>>> downloading regions.....")
-dataset_url_regions <- "https://www.gis-support.pl/downloads/2022/gminy.zip"
-dataset_path_regions <- paste(data_folder, basename(dataset_url_regions), sep = "/")
-dataset_folder_regions <- sub(".zip", "", paste(data_folder, basename(dataset_url_regions), sep = "/"))
-
-if (! file.exists(dataset_path_regions)) {
-	download.file(
-		url = dataset_url_regions,
-		destfile = dataset_path_regions,
-		mode = "wb"
-	)
-
-	unzip(
-		paste(data_folder, basename(dataset_url_regions), sep = "/"),
-		exdir = dataset_folder_regions
-	)
-}
-
-filename_regions <- list.files(
-    path = dataset_folder_regions,
-    pattern = ".shp",
-    full.names = T
+dataset_regions <- POMUtils::fetch_zip_with_shp(
+  env_folders = folders,
+  dataset_url = "https://www.gis-support.pl/downloads/2022/gminy.zip"
 )
+regions <- st_read(dataset_regions, options = "ENCODING=UTF8")
 
 print(">>>>> plotting map.....")
 
-rivers <- st_read(filename_rivers, options = "ENCODING=UTF8")
 rivers_ordered <- rivers[order(rivers$DLUG, decreasing=TRUE),]
 vistula <- head(rivers_ordered, 1)
 
@@ -105,9 +43,8 @@ ggplot() +
 	theme_bw() +
 	labs(title = "Poland with Vistula and Warsaw", x = "Longitude", y = "Latitude", color = "Poland")
 
-ggsave(paste(output_maps_folder, "05-Poland-Vistula-Warsaw.png", sep = "/"))
+ggsave(paste(folders$final_map_folder, "05-Poland-Vistula-Warsaw.png", sep = "/"))
 
-regions <- st_read(filename_regions, options = "ENCODING=UTF8")
 warsaw_region <- regions[regions$JPT_NAZWA_ == "Warszawa",]
 warsaw_region$NAME <- "Warsaw"
 warsaw_region <- st_transform(warsaw_region, st_crs(vistula))
@@ -119,7 +56,7 @@ ggplot() +
 	theme_bw() +
 	labs(title = "Poland with Vistula and Warsaw", x = "Longitude", y = "Latitude", fill = "Poland")
 
-ggsave(paste(output_maps_folder, "05-Poland-Vistula-Warsaw-region.png", sep = "/"))
+ggsave(paste(folders$final_map_folder, "05-Poland-Vistula-Warsaw-region.png", sep = "/"))
 
 warsaw_box <- st_bbox(warsaw_region)
 vistula_boxed <- st_crop(vistula, warsaw_box)
@@ -130,7 +67,7 @@ ggplot() +
 	theme_bw() +
 	labs(title = "Vistula and Warsaw", x = "Longitude", y = "Latitude", fill = "Poland")
 
-ggsave(paste(output_maps_folder, "05-Vistula-Warsaw-region.png", sep = "/"))
+ggsave(paste(folders$final_map_folder, "05-Vistula-Warsaw-region.png", sep = "/"))
 
 krakow_region <- regions[startsWith(regions$JPT_NAZWA_, "Krak"),]
 krakow_region$NAME <- "Krakow"
@@ -142,9 +79,9 @@ ggplot() +
 	geom_sf(data = krakow_region, aes(fill = NAME)) +
 	geom_sf(data = vistula_boxed, color = "blue", linewidth = 1) +
 	theme_bw() +
-	labs(title = "Krakow and Warsaw", x = "Longitude", y = "Latitude", fill = "Poland")
+	labs(title = "Vistula and Krakow", x = "Longitude", y = "Latitude", fill = "Poland")
 
-ggsave(paste(output_maps_folder, "05-Vistula-Krakow-region.png", sep = "/"))
+ggsave(paste(folders$final_map_folder, "05-Vistula-Krakow-region.png", sep = "/"))
 
 print(">>>>> Done.")
 
