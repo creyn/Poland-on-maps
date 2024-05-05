13a5a27403a6135cfb564f6daf063481a619efc0
creyn <creyn@users.noreply.github.com>
2024-05-05 12:27:49 +0200

    Warsaw with tiles, border and the Vistula river

---
+++ src/15-Warsaw-detailed/15-Warsaw-detailed.R
@@ -0,0 +1,63 @@
+print("Create map of Warsaw with Vistula and Leaflet")
+
+install.packages("src/packages/POMUtils_1.1.tar.gz")
+folders <- POMUtils::setup(
+  script_folder = "src/15-Warsaw-detailed/",
+  data_folder = "data/",
+  packages = c("sf", "here", "tidyverse", "ggspatial", "prettymapr")
+)
+
+print(">>>>> downloading rivers.....")
+dataset_rivers <- POMUtils::fetch_zip_with_dataset(
+  env_folders = folders,
+  dataset_url = "http://envirosolutions.pl/dane/rzekiPL.zip"
+)
+rivers <- st_read(dataset_rivers, options = "ENCODING=UTF8")
+
+print(">>>>> downloading regions.....")
+dataset_regions <- POMUtils::fetch_zip_with_dataset(
+  env_folders = folders,
+  dataset_url = "https://www.gis-support.pl/downloads/2022/gminy.zip"
+)
+regions <- st_read(dataset_regions, options = "ENCODING=UTF8")
+
+print(">>>>> processing ....")
+
+rivers_ordered <- rivers[order(rivers$DLUG, decreasing=TRUE),]
+vistula <- head(rivers_ordered, 1)
+
+# poland <- gisco_get_countries(
+# 	country = "PL",
+# 	resolution = "1"
+# )
+
+# Warsaw - latitude = 52.237049, longitude = 21.017532
+# points <- data.frame(lon = c(21.017532), lat = c(52.237049))
+# points$NAME <- c("Warsaw")
+# cities <- st_as_sf(x = points, coords = c("lon", "lat"), crs = 4326)
+
+warsaw_region <- regions[regions$JPT_NAZWA_ == "Warszawa",]
+# warsaw_region$NAME <- "Warsaw"
+warsaw_region <- st_transform(warsaw_region, st_crs(vistula))
+
+warsaw_box <- st_bbox(warsaw_region)
+vistula_boxed <- st_crop(vistula, warsaw_box)
+
+print(">>>>> plotting.....")
+ggplot() +
+    annotation_map_tile("osm", zoom = 11, forcedownload = FALSE) +
+	geom_sf(data = warsaw_region, fill = alpha("#FFFFFF", 0), linewidth = 1) +
+	geom_sf(data = vistula_boxed, color = "blue", linewidth = 1) +
+	theme_void()
+	# labs(title = "Vistula and Warsaw", x = "Longitude", y = "Latitude", fill = "Poland")
+
+ggsave(paste(folders$final_map_folder, "15-Warsaw-detailed.png", sep = "/"))
+
+print(">>>>> Done.")
+
+# to run using Docker
+# docker pull creyn/poland-on-maps:latest
+# OR
+# docker build -t poland-on-maps .
+# docker run -it -v ${PWD}:/home/docker -w /home/docker -e POM_DATA_FOLDER=/home/docker/data -e POM_OUTPUT_MAPS_FOLDER=/home/docker/output poland-on-maps bash
+# Rscript src/15-Warsaw-detailed/15-Warsaw-detailed.R
